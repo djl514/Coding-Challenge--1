9d3e8c936d0ab2545f78f0598f02ee3b8cfeb2e6
David Layne <davidlayne@usf.edu>
2024-08-30 11:30:34 -0400

    Task 1: Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Joshua";
+const employeeID = 430;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
+
