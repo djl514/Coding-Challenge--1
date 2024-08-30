9ab8c5b29a1a3aff28a27e3a8c754f3b29110feb
David Layne <davidlayne@usf.edu>
2024-08-30 11:52:35 -0400

    Task 2: Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -9,3 +9,11 @@ console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
 
 
+// Task 2: Compound Data Types
+let products = ["Iphone", "Ipad", "Apple Watch"];
+const productDetails = {
+    price: 300,
+    inStock: true,
+    name: products
+}
+
